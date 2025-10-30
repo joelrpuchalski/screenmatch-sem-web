f48c95e10287c63f1b7606678cd6540851e6d398
joelrpuchalski <112983728+joelrpuchalski@users.noreply.github.com>
2025-10-30 14:42:20 -0300

    Adicionando lambdas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
@@ -27,13 +28,22 @@ public class Principal {
 
         List<DadosTemporada> temporadas =  new ArrayList<>();
 
-            for (int temporada = 1; temporada <= dadosSerie.totalTemporadas(); temporada++) {
-                json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + temporada + "&apikey=" + API_KEY);
-                var dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-                temporadas.add(dadosTemporada);
+        for (int temporada = 1; temporada <= dadosSerie.totalTemporadas(); temporada++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + temporada + "&apikey=" + API_KEY);
+            var dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
 	}
 
         temporadas.forEach(System.out::println);
+
+        // for(int i = 0; i < dadosSerie.totalTemporadas(); i++) {
+        //     List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+        //     for (int j = 0; j < episodiosTemporada.size(); j++) {
+        //         System.out.println(episodiosTemporada.get(j).titulo());
+        //     }
+        // }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 
 }
